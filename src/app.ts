1c93da5f70733eb0e31ddadca8784089587ac742
Oriz Wahyu N <oriezt.id@gmail.com>
2024-02-04 20:21:25 +0700

    feat: users management (crud)

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,75 @@ import Koa from "koa";
 import Router from "koa-router";
 import bodyParser from "koa-bodyparser";
 import dotenv from "dotenv";
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 dotenv.config();
 const app = new Koa();
 const router = new Router();
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 const PORT = process.env.APP_PORT;
 
 app.use(bodyParser());
 
-router.get("/", async (ctx) => {
+router.get("/", (ctx) => {
   ctx.body = {
     status: true,
     message: "Hello Koa!",
+  };
+});
+
+router.get("/users", async (ctx) => {
+  const users = await prisma.user.findMany();
+  ctx.body = users;
+});
+
+type RegisterUser = {
+  name: string;
+  username: string;
+  password: string;
+};
+
+router.post("/users", async (ctx) => {
+  const { name, username, password }: RegisterUser = ctx.request.body as RegisterUser;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await prisma.user.create({
+    data: {
+      name,
+      username,
+      password: hashedPassword,
+    },
+  });
+  ctx.body = newUser;
+});
+
+router.get("/users/:id", async (ctx) => {
+  const id = Number(ctx.params.id);
+  const user = await prisma.user.findUnique({ where: { id } });
+  ctx.body = user;
+});
+
+type UpdateUser = {
+  name?: string;
+  username?: string;
+};
+
+router.patch("/users/:id", async (ctx) => {
+  const id = Number(ctx.params.id);
+  const { name, username }: UpdateUser = ctx.request.body as UpdateUser;
+  const updatedUser = await prisma.user.update({
+    where: { id },
+    data: { name, username },
+  });
+  ctx.body = updatedUser;
+});
+
+router.delete("/users/:id", async (ctx) => {
+  const id = Number(ctx.params.id);
+  await prisma.user.delete({ where: { id } });
+  ctx.body = {
+    status: true,
+    message: `User with id ${id} deleted successfully`,
   };
 });
 
@@ -24,6 +79,6 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-// process.on("SIGTERM", () => {
-//   prisma.$disconnect();
-// });
+process.on("SIGTERM", () => {
+  prisma.$disconnect();
+});
